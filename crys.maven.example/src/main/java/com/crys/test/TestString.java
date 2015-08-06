559bcc3a13bec8fe6bd39e28fadfa932881c4ad8
pistolove <pistolove@163.com>
2015-08-06 16:02:45 +0800

    test
    Change-Id: Iaf6a56e67d7cc71e999196a0008b282fe76cab0c
    Signed-off-by: pistolove <pistolove@163.com>

---
+++ crys.maven.example/src/main/java/com/crys/test/TestString.java
@@ -0,0 +1,16 @@
+/*package com.crys.test;
+
+import org.junit.Test;
+
+public class TestString {
+
+	@Test
+	public void testString() throws Exception{
+		byte[] c = {123, 34, 105, 100, 34, 58, 49, 44, 34, 110, 97, 109, 101, 34, 58, 34, 108, 105, 103, 97, 110, 103, 34, 125};
+		
+		String str = new String(c, "utf-8");
+		
+		System.err.println(str);
+	}
+}
+*/
